7737c323a673954a3be92c60b6a5a0d8d388de66
Jilin Liu <jilin@oomba.com>
2018-06-09 10:32:43 -0700

    hook components to outlet

--- TwitterClientDemoApp/LoginViewController.swift
+++ TwitterClientDemoApp/LoginViewController.swift
@@ -10,19 +10,31 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var showPasswordButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var coverView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
+    private func login() {
+        // TODO
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func showPasswordButtonClicked(_ sender: UIButton) {
+        togglePassword()
     }
-    */
-
+    
+    private func togglePassword() {
+        //TODO
+        print("Toggle password")
+    }
+    @IBAction func loginButtonClicked(_ sender: UIButton) {
+        coverView.isHidden = false
+        login()
+    }
 }
