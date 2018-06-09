013d1286bca72fe396fe9c7fe519489c5e5ea419
Jilin Liu <jilin@oomba.com>
2018-06-09 10:23:02 -0700

    adjust corner radius and indicator position, add designale uiview extension

---
+++ TwitterClientDemoApp/UIView+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Extension.swift
+//  TwitterClientDemoApp
+//
+//  Created by Jilin Liu on 6/9/18.
+//  Copyright © 2018 JilinStudio, Inc. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class DesignableUIView: UIView {}
+@IBDesignable public class DesignableUIButton: UIButton {}
+
+extension UIView {
+    @IBInspectable
+    public var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        set { layer.cornerRadius = newValue }
+    }
+}
